77b4febc8ae3ce5a9372c29b8b50b8c826d46d07
Grasp CS <info@graspcs.com>
2021-08-10 12:47:56 +0530

    - Added variables - Changed structure

---
+++ Variables/Variables.java
@@ -0,0 +1,23 @@
+class Variables {
+	public static void main(String args[]) {
+		byte a = 10;
+		short b = 20;
+		int c = 30;
+		long d = 40L; // A long value needs to have l or L
+		float e = 50.55f; // A float value needs to have f or F
+		double f = 60.66;
+		boolean g = true;
+		char h = 'a';
+		String i = "Hello World !"; // Here String is a class
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+		System.out.println("e = " + e);
+		System.out.println("f = " + f);
+		System.out.println("g = " + g);
+		System.out.println("h = " + h);
+		System.out.println("i = " + i);
+	}
+}
